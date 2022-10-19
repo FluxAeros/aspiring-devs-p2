565e0b2691dc2f79f3ba358f7a361ce528b678bd
FluxAeros <seanedelen@hotmail.com>
2022-10-19 12:17:46 -0600

    removed script tags

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 window.onload = () => {
     console.log('Your HttpService class should be available on window.httpServer\n\n', window.httpService)
+    document.getElementById()
 }
 
 const http = new HttpService()
